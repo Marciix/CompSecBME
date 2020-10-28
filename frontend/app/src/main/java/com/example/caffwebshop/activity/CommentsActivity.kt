e6b4ea1e805a33a2e30ca6fb7e01a009a45559fc
Imre Gábor <igaborynwa@gmail.com>
2020-10-28 09:56:43 +0100

    Post comments

--- frontend/app/src/main/java/com/example/caffwebshop/activity/CommentsActivity.kt
+++ frontend/app/src/main/java/com/example/caffwebshop/activity/CommentsActivity.kt
@@ -12,10 +12,14 @@ import com.example.caffwebshop.model.CommentPublic
 import kotlinx.android.synthetic.main.activity_comments.*
 import kotlinx.android.synthetic.main.content_comments.*
 import com.example.caffwebshop.R
+import com.example.caffwebshop.fragment.CommentDialogFragment
+import com.example.caffwebshop.model.CommentCreationModel
+import com.example.caffwebshop.model.IdResult
 import com.example.caffwebshop.network.CAFFInteractor
 
 
-class CommentsActivity : AppCompatActivity() {
+class CommentsActivity : AppCompatActivity(),CommentDialogFragment.CommentCreationListener {
+
     private lateinit var token: String
     private var id=0
     private val caffInteractor= CAFFInteractor()
@@ -25,11 +29,14 @@ class CommentsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comments)
-        setSupportActionBar(toolbar)
-
         token=intent.getStringExtra("token")
         id=intent.getIntExtra("id", 0)
         rv_comments.layoutManager=LinearLayoutManager(applicationContext)
+
+        fab_comment.setOnClickListener {
+            val commentDialogFragment = CommentDialogFragment()
+            commentDialogFragment.show(supportFragmentManager,"TAG")
+        }
 
         val url = "https://caffshop-api.nkelemen.hu/caffitems/$id/preview.jpg"
         val glideUrl = GlideUrl(url) { mapOf(Pair("Authorization", token)) }
@@ -42,6 +49,8 @@ class CommentsActivity : AppCompatActivity() {
 
         //withauthors added
         caffInteractor.getCaffItemsByIDComment(token=token, param=id,withAuthors = true, onSuccess = this::onLoadCommentsSuccess, onError = this::onLoadCommentsError)
+
+
     }
 
     private fun onLoadCommentsSuccess(list: List<CommentPublic>){
@@ -53,5 +62,21 @@ class CommentsActivity : AppCompatActivity() {
         e.printStackTrace()
         Toast.makeText(applicationContext, "Unable to load comments!", Toast.LENGTH_LONG).show()
     }
+
+
+    override fun onCommentCreated(comment: String) {
+        val c=CommentCreationModel(comment)
+        caffInteractor.commentCaffItem(token, id,c,this::onCommentSuccess,this::onCommentError)
+    }
+
+    private fun onCommentSuccess(res: IdResult){
+        caffInteractor.getCaffItemsByIDComment(token=token, param=id,withAuthors = true, onSuccess = this::onLoadCommentsSuccess, onError = this::onLoadCommentsError)
+
+    }
+    private fun onCommentError(e:Throwable){
+        Toast.makeText(applicationContext, "Unable to add comments!", Toast.LENGTH_LONG).show()
+        e.printStackTrace()
+    }
+
 
 }
