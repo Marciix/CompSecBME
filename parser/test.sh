1863b7d4d7631e4cb1e866b5ce7abba3b38dcf5d
Bak Marton Laszlo <bak.marton97@gmail.com>
2020-11-29 02:37:11 +0100

    AFL tests added

--- parser/test.sh
+++ parser/test.sh
@@ -1,5 +1,4 @@
 #run with source test.sh
 make build-test
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/marcix/CompSecBME/parser/Release
-afl-g++ -L/home/marcix/CompSecBME/parser/Release -Wall -O2 main.cpp -lcaff -o test.bin
-afl-fuzz -i ./test_files -o output test.bin @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/martonbak/CompSecBME/parser/Release
+/home/martonbak/Tools/afl-2.52b/afl-g++ -L/home/martonbak/CompSecBME/parser/Release -Wall -O2 main.cpp -lcaff -o parser.bin
